e64e8400d7ed7452d61d0c5bd7fcd9ecf2caa666
Elementary OS Hera 219 <realgarcilaso@outlook.com>
2021-09-28 06:46:40 -0500

    11.2  Question: colored square - status set color statict
    green gb

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -18,7 +18,7 @@
                                         {{ \Str::plural('vote', $question->votes) }}
                                     </div>
 
-                                    <div class="status">
+                                    <div class="status answered-accepted">
                                         <strong>
                                             {{ $question->answers }}
                                         </strong>
